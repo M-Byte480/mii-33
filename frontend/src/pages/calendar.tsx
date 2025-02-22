9d3cb21f52492583deb36f7dfb418e7379a58ab1
Milan <kmilanm13@gmail.com>
2025-02-22 23:51:16 +0000

    [mialn ] jwt now pulls calendar

--- frontend/src/pages/calendar.tsx
+++ frontend/src/pages/calendar.tsx
@@ -1,4 +1,22 @@
+import { Session } from "inspector";
+import { useEffect, useState } from "react";
+import { getSessionItem } from "../App";
+import { jwtDecode } from "jwt-decode";
+
 export function Calendar() {
+    const [session, setSession] = useState<string | null>(null);
+    useEffect(() => {
+        const sessionData  = getSessionItem("jwt");
+        if (sessionData) {
+            const decoded = jwtDecode(sessionData);
+            // @ts-ignore
+            const encodedEmail = encodeURIComponent(decoded.email);
+            const sessionCalendar = `https://calendar.google.com/calendar/embed?src=${encodedEmail}&ctz=Europe%2FDublin&mode=WEEK`;
+            setSession(sessionCalendar);
+        }
+    }
+    , []);
+
     return (
         <>
             <div className="min-h-screen bg-gray-100">
@@ -7,16 +25,20 @@ export function Calendar() {
                     <h1 className="text-4xl font-bold">Our Google Calendar</h1>
                     <p className="mt-4 text-lg">Stay updated with our latest schedules and events.</p>
                 </header>
-
                 {/* Calendar Section */}
-                <section className="py-20 px-6 max-w-5xl mx-auto text-center">
-                    <h2 className="text-3xl font-bold text-gray-800">Upcoming Events</h2>
-                    <div className="mt-10 bg-white p-6 rounded-lg shadow-md">
-                        <iframe src="https://calendar.google.com/calendar/embed?src=02d404df99bbbb581dd1e66d6951fb0ddc70906925bad545800a3359e5256935%40group.calendar.google.com&ctz=Europe%2FDublin&mode=WEEK"
-                            width="800" height="600"
-                            scrolling="no"></iframe>
-                    </div>
-                </section>
+                {session ? (
+                    <section className="py-20 px-6 max-w-5xl mx-auto text-center">
+                        <h2 className="text-3xl font-bold text-gray-800">Upcoming Events</h2>
+                        <div className="mt-10 bg-white p-6 rounded-lg shadow-md">
+                            <iframe src={ session }
+                                width="800" height="600"
+                                scrolling="no"></iframe>
+                        </div>
+                    </section>
+                ) : (
+                    <></>
+                )}
+
             </div>
         </>
     )
