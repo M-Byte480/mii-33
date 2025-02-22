55b620ea3d5138bc09863daf9e15ad879afda94e
DJK101 <danieljeffersonkennedy@gmail.com>
2025-02-22 23:31:25 +0000

    Add jwt endpoint

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    # Expecting the token to be in the Authorization header formatted as "Bearer <token>"
+    header = request.headers["Authorization"]
+    token = header.split(" ").last if header
+
+    begin
+      decoded = JsonWebToken.decode(token)
+      @current_user = User.find(decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => _
+      render json: { error: "Not Authorized" }, status: :unauthorized
+    end
+  end
 end
