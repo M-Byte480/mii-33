de7c82bd1f07b816104b4844641c51e0c22d50af
Milan <kmilanm13@gmail.com>
2025-02-22 18:21:29 +0000

    [Milan] some frontend changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Home } from './pages/home';
+import { Calendar } from './pages/calendar';
+import { Dashboard } from './pages/dashboard';
+import { LoginPage } from './pages/login';
 
 function App() {
+  const calendarID = process.env.REACT_APP_CALENDAR_ID
+  const apiKey = process.env.REACT_APP_GOOGLE_API_KEY
+  const accessToken = process.env.REACT_APP_GOOGLE_ACCESS_TOKEN
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav className="bg-blue-600 text-white py-4">
+        <div className="max-w-5xl mx-auto flex justify-between px-6">
+          <h1 className="text-xl font-bold">MyApp</h1>
+          <div className="space-x-6">
+            <Link to="/" className="hover:underline">Home</Link>
+            <Link to="/calendar" className="hover:underline">Calendar</Link>
+            <Link to="/dashboard" className="hover:underline">Dashboard</Link>
+            <Link to="/login" className="hover:underline">Login</Link>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
