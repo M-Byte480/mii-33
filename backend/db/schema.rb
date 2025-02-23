34015d6e69840edd2e87d2f59018e0eb8637f0ee
DJK101 <danieljeffersonkennedy@gmail.com>
2025-02-23 00:24:06 +0000

    Update migrations

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,25 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_214733) do
-  create_table "events", force: :cascade do |t|
-    t.datetime "start_time"
-    t.datetime "end_time"
-    t.integer "duration"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "summary"
-  end
-
+ActiveRecord::Schema[8.0].define(version: 2025_02_22_221243) do
   create_table "schedules", force: :cascade do |t|
     t.integer "event_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index [ "event_id" ], name: "index_schedules_on_event_id"
+    t.index ["event_id"], name: "index_schedules_on_event_id"
   end
 
-  add_foreign_key "schedules", "events"
-  
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
@@ -36,4 +25,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_22_214733) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "schedules", "events"
 end
