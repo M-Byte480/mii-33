147b61e1bf16931ca1dc206831b9ee5fc32dcf5b
ArtLiathain <artp.oliathain@gmail.com>
2025-02-23 06:57:04 +0000

    feature: add get meeting times@

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -20,11 +20,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_23_000001) do
     t.index ["user_id"], name: "index_guests_on_user_id"
   end
 
-  create_table "schedules", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "first_name"
