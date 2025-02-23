c286e4e1da9f6174117b045f82926fe52ea6baa9
DJK101 <danieljeffersonkennedy@gmail.com>
2025-02-23 05:19:35 +0000

    Add email to mii-JWT

--- backend/app/controllers/session_controller.rb
+++ backend/app/controllers/session_controller.rb
@@ -18,8 +18,8 @@ class SessionController < ApplicationController
       # Update the user's first_name and last_name if they already exist
       user.update(first_name: user_info["given_name"], last_name: user_info["family_name"])
 
-      jwt = JsonWebToken.encode(user_id: user.id)
-      render json: { token: jwt }, status: :ok
+      jwt = JsonWebToken.encode(user_id: user.id, email: user.email)
+      render json: { token: jwt, first_name: user_info["given_name"] }, status: :ok
     else
       render json: { error: "Invalid token" }, status: :unauthorized
     end
