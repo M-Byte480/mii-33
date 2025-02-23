248befe091473bd78a63f76368c73248bb36ac8e
Milan <kmilanm13@gmail.com>
2025-02-23 10:38:46 +0000

    [analysis] milan

--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,6 +1,10 @@
 import { Line } from "react-chartjs-2"
 import { Chart as ChartJS, TimeScale, LinearScale, PointElement, LineElement, Tooltip, Legend } from "chart.js";
 import "chartjs-adapter-date-fns";
+import Button from "@mui/material/Button";
+import { CSSProperties, useState } from "react";
+import { getLocalStorageItem } from "../funcs/storage";
+import { ClipLoader } from "react-spinners";
 // Register necessary Chart.js components
 ChartJS.register(TimeScale, LinearScale, PointElement, LineElement, Tooltip, Legend);
 
@@ -20,7 +24,62 @@ export function Dashboard() {
 }
 
 export function Graph() {
-  const data = {
+  const [response, setResponse] = useState<string>("Analysis Result");
+  let apiResponse = {}; // todo: query data from api
+  const [loading, setLoading] = useState<boolean>(false);
+  let [color, setColor] = useState("#000000");
+  /*
+    {
+      data: [
+        { start: "2024-02-01T12:00:00Z", end: "2024-02-01T13:00:00Z", meaningful: True, role: d,  },
+        { start: "2024-02-01T12:00:00Z", end: "2024-02-01T13:00:00Z", meaningful: True, role: d,  },
+        { start: "2024-02-01T12:00:00Z", end: "2024-02-01T13:00:00Z", meaningful: True, role: d,  },
+      ]
+    }
+  */
+
+  apiResponse = {
+    "meeting_details": {
+      "topic": "Sprint Planning",
+      "current_meeting_time": "10:00",
+      "duration": 60
+    },
+    "participants": [
+      {
+        "id": "P-001",
+        "role": "Manager",
+        "hourly_rate": 100,
+        "free_time": [
+          "09:00",
+          "10:00"
+        ],
+        "preferred_time": [
+          "09:00",
+          "10:00"
+        ]
+      },
+      {
+        "id": "P-002",
+        "role": "Developer",
+        "hourly_rate": 80,
+        "free_time": [
+          "10:00",
+          "11:00"
+        ],
+        "preferred_time": [
+          "10:00",
+          "11:00"
+        ]
+      }
+    ],
+    "additional_metrics": {
+      "flow_time_interrupted": 15,
+      "previous_meetings_effectiveness": 75,
+      "calculated_meeting_cost": 90.0
+    }
+  };
+
+  let data = {
     labels: [
       "2024-02-01T00:00:00Z",
       "2024-02-02T00:00:00Z",
@@ -69,9 +128,53 @@ export function Graph() {
     },
   };
 
+  const handleClick = () => {
+    setLoading(true);
+    fetch("http://localhost:3001/chat/analysis", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + getLocalStorageItem("mii-jwt")
+      },
+      body: JSON.stringify({
+        data: apiResponse
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setResponse(data.analysis);
+      }).finally(() => {
+        setLoading(false);
+      });;
+  }
+
+  const parseResponse = (response: string) => {
+    return response.replaceAll("#", "").replaceAll("*", "").replace(/(?:\r\n|\r|\n)/g, '<br>');
+  }
+
+  const override: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
   return (
     <>
       <h2>Time Scale Graph (Chart.js 2)</h2>
+      <Button onClick={handleClick}>Analyse</Button>
+      {loading && (
+        <ClipLoader
+          color={color}
+          loading={loading}
+          cssOverride={override}
+          size={50}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      )}
+      <p className="p4" dangerouslySetInnerHTML={{ __html: parseResponse(response) }} />
+
+
       {/* @ts-ignore */}
       <Line data={data} options={options} />
     </>
